275eeedfd5d9b3e56344ae8e981219617e634255
ian <speegie.ian@gmail.com>
2017-04-27 09:48:02 -0400

    Made Classes and XML's for Tabs - This is an outline, not the tabs we will actually be using.

--- app/src/main/java/com/morasclass/mora/hiitblast/Tab1Contacts.java
+++ app/src/main/java/com/morasclass/mora/hiitblast/Tab1Contacts.java
@@ -9,7 +9,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+
 
 public class Tab1Contacts extends Fragment {
 
