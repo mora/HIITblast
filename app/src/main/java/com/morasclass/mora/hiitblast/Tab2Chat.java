133630f549130c8386d166d61b13733d0b3b27c0
n080873 <janfc6@gmail.com>
2017-05-23 12:27:20 -0400

    jan

--- app/src/main/java/com/morasclass/mora/hiitblast/Tab2Chat.java
+++ app/src/main/java/com/morasclass/mora/hiitblast/Tab2Chat.java
@@ -1,10 +1,13 @@
 package com.morasclass.mora.hiitblast;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 /**
  * Created by n398447 on 4/20/2017.
@@ -12,10 +15,50 @@ import android.view.ViewGroup;
 
 public class Tab2Chat extends Fragment {
 
+    Handler timerHandler = new Handler();
+    long startTime = 0;
+    TextView timerView;
+
+    Runnable timerRunnable = new Runnable() {
+
+        @Override
+        public void run() {
+            long millis = System.currentTimeMillis() - startTime;
+            int seconds = (int) (millis / 1000);
+            int minutes = seconds / 60;
+            seconds = seconds % 60;
+
+            timerView.setText(String.format("%d:%02d", minutes, seconds));
+            timerHandler.postDelayed(this, 500);
+        }
+    };
+
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.tab2chat, container, false);
+        timerView = (TextView) rootView.findViewById(R.id.timerTextView);
+
+        Button b = (Button) rootView.findViewById(R.id.timerBtn);
+        b.setText("start");
+        b.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Button b = (Button) v;
+                if (b.getText().equals("stop")) {
+                    timerHandler.removeCallbacks(timerRunnable);
+                    b.setText("start");
+                } else {
+                    startTime = System.currentTimeMillis();
+                    timerHandler.postDelayed(timerRunnable, 0);
+                    b.setText("stop");
+                }
+            }
+        });
         return rootView;
     }
+
+    //add override to super.onpause
+    //https://stackoverflow.com/questions/4597690/android-timer-how-to
+
 }
